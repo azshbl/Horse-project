d834081e7dd7bfa56dccaab0e93eed8825cdec52
nawafdhowihi <nawafdhowihi@gmail.com>
2026-06-21 22:20:40 +0300

    the new changes

--- HP.py
+++ HP.py
@@ -1,5 +1,5 @@
 horses = {
-    1:{"owner_name" : "Nawaf" ,"horse_name ": "Ali" ,"age":4  ,  "ginder" : "male"  , "breed" : "" , "status" : }
+    1:{"owner_name" : "Nawaf" ,"horse_name ": "Ali" ,"age":4  ,  "ginder" : "male"  , "breed" : "" , "status" : ""}
     2:{"owner_name":"Turki" ,"horse_name ": "alking" ," age ": 6 , "ginder" : "female" , "breed" : "" , "status" : }
     3:{}
     4:{}
@@ -33,13 +33,27 @@ trainee = {
 }
 stable_orders = []
 
+def main_menu():
+  while:
+    print("===== Horse Stable System =====")
+    print("1.Admin")
+    print("2.Trainee")
+    print("3.Horse Owner ")
+    print("4.Exit")
+    
+    choice = int(input("Choose your role: "))
 
+    if choice == 1 :
+      admin_menu()
+    elif choice == 2 :
+      trainee_menu()
+    elif choice == 3 :
+      horse_owner_menu()
+    elif choice == 4 :
+      break
+    else:
+      print("Invalid input!!")
 
-
-def main()
-def trainee()     
-def admin()
-def horse_owner()  
 
 
 
