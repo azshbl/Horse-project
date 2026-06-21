56a1463d0ae391e14322636f73ec4d4d7997cf7d
nawafdhowihi <nawafdhowihi@gmail.com>
2026-06-21 21:12:18 +0300

    Initialize horse and trainee data structure
    Added initial data structures for horses and trainees.

--- HP.py
+++ HP.py
@@ -1,1 +1,45 @@
+horses = {
+    1:{"owner_name" : "Nawaf" ,"horse_name ": "Ali" ,"age":4  ,  "ginder" : "male"  , "breed" : "" , "status" : }
+    2:{"owner_name":"Turki" ,"horse_name ": "alking" ," age ": 6 , "ginder" : "female" , "breed" : "" , "status" : }
+    3:{}
+    4:{}
+    5:{}
+    6:{}
+    7:{}
+    8:{}
+    9:{}
+    10:{}
+    11:{}
+    12:{}
+    13:{}
+    14:{}
+    15:{}
+    16:{}
+    17:{}
+    18:{}
+    19:{}
+    20:{}
+}
+
+trainee = {
+    1:{}
+    2:{}
+    3:{}
+    4:{}
+    5:{}
+    6:{}
+    7:{}
+    8:{}
+}
+stable_orders = []
+
+
+
+
+def main()
+def trainee()     
+def admin()
+def horse_owner()  
+
+
 
