9e81216a60e2b7ef77fbb549f27c44220d308d84
turki Abuhaimed <torki112231@gmail.com>
2026-06-23 06:08:37 +0300

    turki's part admin.py

---
+++ admin.py
@@ -0,0 +1,389 @@
+admin method
+
+horses = {
+    1: {"owner_name": "Turki", "horse_name": "Wadah", "age": 6, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 100, "health_score": 100},
+    2: {"owner_name": "Turki", "horse_name": "Shamekh", "age": 5, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 97, "health_score": 95},
+    3: {"owner_name": "Turki", "horse_name": "Alskb", "age": 7, "gender": "male", "breed": "frezian", "status": "stable", "horse_level": 94, "health_score": 92},
+    4: {"owner_name": "Turki", "horse_name": "Abiah.t", "age": 7, "gender": "female", "breed": "arabian", "status": "stable", "horse_level": 96, "health_score": 98},
+    5: {"owner_name": "Turki", "horse_name": "Seham", "age": 5, "gender": "female", "breed": "arabian", "status": "stable", "horse_level": 91, "health_score": 93},
+    6: {"owner_name": "Turki", "horse_name": "Rahaf", "age": 6, "gender": "female", "breed": "arabian", "status": "stable", "horse_level": 72, "health_score": 96},
+    7: {"owner_name": "Turki", "horse_name": "Kehelan", "age": 4, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 72, "health_score": 89},
+    8: {"owner_name": "Turki", "horse_name": "shujaa", "age": 8, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 99, "health_score": 99},
+    9: {"owner_name": "Turki", "horse_name": "Borkan", "age": 5, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 93, "health_score": 94},
+    10: {"owner_name": "Turki", "horse_name": "Raad", "age": 4, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 92, "health_score": 90},
+    11: {"owner_name": "Turki", "horse_name": "Barq", "age": 7, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 75, "health_score": 99},
+    12: {"owner_name": "Turki", "horse_name": "Asael", "age": 6, "gender": "female", "breed": "arabian", "status": "stable", "horse_level": 77, "health_score": 95},
+    13: {"owner_name": "Nawaf", "horse_name": "Mazyon", "age": 5, "gender": "male", "breed": "arabian", "status": "boarding", "horse_level": 88, "health_score": 84},
+    14: {"owner_name": "Abdulaziz", "horse_name": "alshqh", "age": 4, "gender": "male", "breed": "arabian", "status": "boarding", "horse_level": 85, "health_score": 87},
+    15: {"owner_name": "Nawaf.z", "horse_name": "gzar", "age": 6, "gender": "male", "breed": "frezian", "status": "boarding", "horse_level": 89, "health_score": 91},
+    16: {"owner_name": "Abdullah", "horse_name": "nomas", "age": 5, "gender": "male", "breed": "arabian", "status": "boarding", "horse_level": 100, "health_score": 100},
+    17: {"owner_name": "Khulod", "horse_name": "Najm", "age": 7, "gender": "male", "breed": "arabian", "status": "boarding", "horse_level": 100, "health_score": 100},
+    18: {"owner_name": "Abdulaziz", "horse_name": "abiah", "age": 4, "gender": "female", "breed": "arabian", "status": "boarding", "horse_level": 74, "health_score": 90},
+    19: {"owner_name": "Nawaf", "horse_name": "Anood", "age": 5, "gender": "female", "breed": "arabian", "status": "boarding", "horse_level": 70, "health_score": 85},
+    20: {"owner_name": "Nawaf.z", "horse_name": "Shadad", "age": 6, "gender": "male", "breed": "frezian", "status": "boarding", "horse_level": 65, "health_score": 89}
+}
+
+
+trainers = {
+    1: {"trainer_name": "Turki", "age": 24, "training_type": "Jumping, For fun,", "sessions_completed": 70, "performance_score": 99},
+    2: {"trainer_name": "Abdulaziz", "age": 24, "training_type": "Jumping", "sessions_completed": 56, "performance_score": 95},
+    3: {"trainer_name": "Nawaf.z", "age": 24, "training_type": "For fun", "sessions_completed": 20, "performance_score": 95},
+    4: {"trainer_name": "Nawaf", "age": 24, "training_type": "Racing", "sessions_completed": 50, "performance_score": 90},
+    5: {"trainer_name": "Khalid", "age": 22, "training_type": "Racing", "sessions_completed": 30, "performance_score": 85},
+    6: {"trainer_name": "Saud", "age": 20, "training_type": "For fun", "sessions_completed": 10, "performance_score": 80},
+    7: {"trainer_name": "Abdullah", "age": 24, "training_type": "Jumping", "sessions_completed": 22, "performance_score": 75},
+    8: {"trainer_name": "Mohammed", "age": 26, "training_type": "For fun", "sessions_completed": 40, "performance_score": 70}
+}
+
+
+
+owners = {
+    1: {"owner_name": "Turki", "phone": "05012345671", "city": "Riyadh", "owner_type": "Stable Owner"},
+    2: {"owner_name": "Nawaf", "phone": "05012345681", "city": "Riyadh", "owner_type": "Boarding Client"},
+    3: {"owner_name": "Abdulaziz", "phone": "05012345691", "city": "Riyadh", "owner_type": "Boarding Client"},
+    4: {"owner_name": "Nawaf.z", "phone": "05012345701", "city": "Riyadh", "owner_type": "Boarding Client"},
+    5: {"owner_name": "Abdullah", "phone": "05012345711", "city": "Riyadh", "owner_type": "Boarding Client"},
+    6: {"owner_name": "Khulod", "phone": "05012345721", "city": "Riyadh", "owner_type": "Boarding Client"},
+    7: {"owner_name": "Abulaziz", "phone": "05012345731", "city": "Riyadh", "owner_type": "Boarding Client"}
+}
+
+
+trainees = {
+    1: {"trainee_name": "Ahmed", "age": 20, "training_type": "Jumping", "completed_classes": 5, "package": 10, "license": False},
+    2: {"trainee_name": "Mohammed", "age": 23, "training_type": "Racing", "completed_classes": 18, "package": 20, "license": False},
+    3: {"trainee_name": "Khalid", "age": 25, "training_type": "For fun", "completed_classes": 35, "package": 40, "license": False},
+    4: {"trainee_name": "Saad", "age": 21, "training_type": "Jumping", "completed_classes": 40, "package": 40, "license": True}
+}
+
+
+
+def show_horses():
+
+    print("\n========== HORSES LIST ==========\n")
+
+    for horse_num, horse in horses.items():
+
+        print(f"Horse Name: {horse['horse_name']}")
+        print(f"Owner: {horse['owner_name']}")
+        print(f"Breed: {horse['breed']}")
+        print(f"Age: {horse['age']}")
+        print(f"Health Score: {horse['health_score']}")
+        print(f"Horse Level: {horse['horse_level']}")
+        print(f"Status: {horse['status']}")
+        print("-" * 30)
+
+
+
+
+def search_horse():
+
+    while True:
+
+        horse_name = input("\nEnter horse name (or 'exit' to stop): ")
+
+        if horse_name.lower() == "exit":
+            break
+
+        for horse_id, horse in horses.items():
+
+            if horse["horse_name"].lower() == horse_name.lower():
+
+                print("\nHorse Found!")
+                print(f"ID: {horse_id}")
+                print(f"Name: {horse['horse_name']}")
+                print(f"Owner: {horse['owner_name']}")
+                print(f"Health Score: {horse['health_score']}")
+                print(f"Horse Level: {horse['horse_level']}")
+
+                break
+
+        else:
+            print("Horse not found.")
+
+
+
+def trainer_level(score):
+
+    if score >= 90:
+        return "Advanced"
+
+    elif score >= 80 and score <=89:
+        return "Intermediate"
+
+    elif score < 80:
+        return "Beginner"
+
+
+
+def show_trainers():
+
+    print("\n===== TRAINERS LIST =====\n")
+
+    for trainer_id, trainer in trainers.items():
+
+        level = trainer_level(trainer["performance_score"])
+
+        print(f"Trainer Name: {trainer['trainer_name']}")
+        print(f"Age: {trainer['age']}")
+        print(f"Training Type: {trainer['training_type']}")
+        print(f"Sessions Completed: {trainer['sessions_completed']}")
+        print(f"Performance Score: {trainer['performance_score']}")
+        print(f"Level: {level}")
+        print("-" * 30)
+
+
+
+def best_trainer():
+
+    best = max(trainers.values(), key=lambda trainer: trainer["performance_score"])
+
+    print("\n===== BEST TRAINER =====")
+    print(f"Trainer Name: {best['trainer_name']}")
+    print(f"Performance Score: {best['performance_score']}")
+    print(f"Sessions Completed: {best['sessions_completed']}")
+    print(f"Level: {trainer_level(best['performance_score'])}")
+
+
+
+
+def show_trainees():
+
+    print("\n===== TRAINEES LIST =====\n")
+
+    for trainee_id, trainee in trainees.items():
+
+        print(f"Trainee ID: {trainee_id}")
+        print(f"Name: {trainee['trainee_name']}")
+        print(f"Age: {trainee['age']}")
+        print(f"Training Type: {trainee['training_type']}")
+        print(f"Completed Classes: {trainee['completed_classes']}")
+        print(f"Package: {trainee['package']}")
+        print(f"License: {trainee['license']}")
+        print("-" * 30)
+
+
+
+def horse_statistics():
+
+    total_horses = len(horses)
+
+    stable_horses_count = 0
+    boarding_count = 0
+    arabian_count = 0
+    frezian_count = 0
+
+    for horse in horses.values():
+
+        if horse["status"] == "stable":
+            stable_horses_count += 1
+
+        elif horse["status"] == "boarding":
+            boarding_count += 1
+
+        if horse["breed"] == "arabian":
+            arabian_count += 1
+
+        elif horse["breed"] == "frezian":
+            frezian_count += 1
+
+    print("\n===== Horse Statistics =====")
+    print(f"Total Horses: {total_horses}")
+    print(f"Stable Horses: {stable_horses_count}")
+    print(f"Boarding Horses: {boarding_count}")
+    print(f"Arabian Horses: {arabian_count}")
+    print(f"frezian Horses: {frezian_count}")
+
+
+
+
+def admin_menu():
+
+    while True:
+
+        print("\n===== ADMIN MENU =====")
+        print("1. Show Horses")
+        print("2. Search Horse")
+        print("3. Horse Statistics")
+        print("4. Show Trainers")
+        print("5. Best Trainer")
+        print("6. Show Trainees")
+        print("0. Back")
+
+        choice = input("\nEnter your choice: ")
+
+        if choice == "1":
+            show_horses()
+
+        elif choice == "2":
+            search_horse()
+
+        elif choice == "3":
+            horse_statistics()
+
+        elif choice == "4":
+            show_trainers()
+
+        elif choice == "5":
+            best_trainer()
+
+        elif choice == "6":
+            show_trainees()
+
+        elif choice == "0":
+            break
+
+        else:
+            print("Invalid choice.")
+
+
+
+
+
+def horse_owner_menu(owner_name):
+
+    while True:
+
+        print(f"\n===== WELCOME {owner_name} =====")
+        print("1. View My Horses")
+        print("0. Back")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+
+            for horse in horses.values():
+
+                if horse["owner_name"].lower() == owner_name.lower():
+
+                    print(f"\nHorse: {horse['horse_name']}")
+                    print(f"Breed: {horse['breed']}")
+                    print(f"Health Score: {horse['health_score']}")
+                    print(f"Horse Level: {horse['horse_level']}")
+
+        elif choice == "0":
+            break
+
+        else:
+            print("Invalid choice")
+
+
+
+
+
+def trainee_menu(trainee_name):
+
+    while True:
+
+        print("1. My Information")
+        print("2. Remaining Classes")
+        print("3. License Status")
+        print("0. Back")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+
+            for trainee in trainees.values():
+
+                if trainee["trainee_name"].lower() == trainee_name.lower():
+
+                    print(f"\nName: {trainee['trainee_name']}")
+                    print(f"Age: {trainee['age']}")
+                    print(f"Training Type: {trainee['training_type']}")
+                    print(f"Completed Classes: {trainee['completed_classes']}")
+                    print(f"Package: {trainee['package']}")
+
+                    if trainee["license"]:
+                       print("License: Licensed")
+                    else:
+                       print("License: Not Licensed")
+                    
+                    break
+
+
+        elif choice == "2":
+
+            for trainee in trainees.values():
+
+                if trainee["trainee_name"].lower() == trainee_name.lower():
+
+                    remaining = trainee["package"] - trainee["completed_classes"]
+
+                    print(f"\nRemaining Classes: {remaining}")
+                    break
+
+        elif choice == "3":
+          
+          for trainee in trainees.values():
+            
+            if trainee["trainee_name"].lower() == trainee_name.lower():
+              
+              if trainee["license"]:
+                
+                print("\nLicense Status: Licensed")
+
+              else:
+                print("\nLicense Status: Not Licensed")
+              
+              break
+
+        elif choice == "0":
+            break
+
+        else:
+            print("Invalid choice")
+
+
+
+
+
+def main_menu():
+
+    while True:
+
+        print("\n===== HORSE STABLE MANAGEMENT SYSTEM =====")
+        print("1. Admin")
+        print("2. Horse Owner")
+        print("3. Trainee")
+        print("0. Exit")
+
+        choice = input("\nEnter your choice: ")
+
+        if choice == "1":
+            admin_menu()
+
+        elif choice == "2":
+
+            owner_name = input("Enter your name: ")
+
+            for owner in owners.values():
+
+                if owner["owner_name"].lower() == owner_name.lower():
+                    horse_owner_menu(owner_name)
+                    break
+
+            else:
+                print("Owner not found.")
+
+        elif choice == "3":
+
+            trainee_name = input("Enter your name: ")
+
+            for trainee in trainees.values():
+
+                if trainee["trainee_name"].lower() == trainee_name.lower():
+                    trainee_menu(trainee_name)
+                    break
+
+            else:
+                print("Trainee not found.")
+
+        elif choice == "0":
+            print("Thank you for using the system.")
+            break
+
+        else:
+            print("Invalid choice.")
+
+
+main_menu()
