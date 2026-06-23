874cc61f5113f60fdbc1e72cbda66d4db9d01f71
Abdulaziz ALsebl <az.alshebl@gmail.com>
2026-06-24 01:16:56 +0300

    Add trainee registration and management system

--- trainee.py
+++ trainee.py
@@ -1,1 +1,335 @@
 trainee window
+
+
+horses = {
+    1: {"owner_name": "Turki", "horse_name": "Wadah", "age": 6, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 100, "health_score": 100},
+    2: {"owner_name": "Turki", "horse_name": "Shamekh", "age": 5, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 97, "health_score": 95},
+    3: {"owner_name": "Turki", "horse_name": "Alskb", "age": 7, "gender": "male", "breed": "frezian", "status": "stable", "horse_level": 94, "health_score": 92},
+    4: {"owner_name": "Turki", "horse_name": "Abiah.t", "age": 7, "gender": "female", "breed": "arabian", "status": "stable", "horse_level": 96, "health_score": 98},
+    5: {"owner_name": "Turki", "horse_name": "Seham", "age": 5, "gender": "female", "breed": "arabian", "status": "stable", "horse_level": 91, "health_score": 93},
+    6: {"owner_name": "Turki", "horse_name": "Rahaf", "age": 6, "gender": "female", "breed": "arabian", "status": "stable", "horse_level": 72, "health_score": 96},
+    7: {"owner_name": "Turki", "horse_name": "Kehelan", "age": 4, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 72, "health_score": 89},
+    8: {"owner_name": "Turki", "horse_name": "shujaa", "age": 8, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 99, "health_score": 99},
+    9: {"owner_name": "Turki", "horse_name": "Borkan", "age": 5, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 93, "health_score": 94},
+    10: {"owner_name": "Turki", "horse_name": "Raad", "age": 4, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 92, "health_score": 90},
+    11: {"owner_name": "Turki", "horse_name": "Barq", "age": 7, "gender": "male", "breed": "arabian", "status": "stable", "horse_level": 75, "health_score": 99},
+    12: {"owner_name": "Turki", "horse_name": "Asael", "age": 6, "gender": "female", "breed": "arabian", "status": "stable", "horse_level": 77, "health_score": 95},
+    13: {"owner_name": "Nawaf", "horse_name": "Mazyon", "age": 5, "gender": "male", "breed": "arabian", "status": "boarding", "horse_level": 88, "health_score": 84},
+    14: {"owner_name": "Abdulaziz", "horse_name": "alshqh", "age": 4, "gender": "male", "breed": "arabian", "status": "boarding", "horse_level": 85, "health_score": 87},
+    15: {"owner_name": "Nawaf.z", "horse_name": "gzar", "age": 6, "gender": "male", "breed": "frezian", "status": "boarding", "horse_level": 89, "health_score": 91},
+    16: {"owner_name": "Abdullah", "horse_name": "nomas", "age": 5, "gender": "male", "breed": "arabian", "status": "boarding", "horse_level": 100, "health_score": 100},
+    17: {"owner_name": "Khulod", "horse_name": "Najm", "age": 7, "gender": "male", "breed": "arabian", "status": "boarding", "horse_level": 100, "health_score": 100},
+    18: {"owner_name": "Abdulaziz", "horse_name": "abiah", "age": 4, "gender": "female", "breed": "arabian", "status": "boarding", "horse_level": 74, "health_score": 90},
+    19: {"owner_name": "Nawaf", "horse_name": "Anood", "age": 5, "gender": "female", "breed": "arabian", "status": "boarding", "horse_level": 70, "health_score": 85},
+    20: {"owner_name": "Nawaf.z", "horse_name": "Shadad", "age": 6, "gender": "male", "breed": "frezian", "status": "boarding", "horse_level": 65, "health_score": 89}
+}
+
+
+trainers = {
+    1: {"trainer_name": "Turki", "age": 24, "training_type": "Racing, For fun,",  "sessions_completed": 70, "performance_score": 99},
+    2: {"trainer_name": "Abdulaziz", "age": 24, "training_type": "Jumping",  "sessions_completed": 56, "performance_score": 95},
+    3: {"trainer_name": "Nawaf.z", "age": 24, "training_type": "For fun", "sessions_completed": 20, "performance_score": 89},
+    4: {"trainer_name": "Nawaf", "age": 24, "training_type": "Racing",  "sessions_completed": 50, "performance_score": 85},
+    5: {"trainer_name": "Khalid", "age": 22, "training_type": "Racing",  "sessions_completed": 30, "performance_score": 80},
+    6: {"trainer_name": "Saud", "age": 20, "training_type": "Racing",  "sessions_completed": 10, "performance_score": 79},
+    7: {"trainer_name": "Abdullah", "age": 24, "training_type": "Jumping",  "sessions_completed": 22, "performance_score": 75},
+    8: {"trainer_name": "Mohammed", "age": 26, "training_type": "For fun",  "sessions_completed": 40, "performance_score": 70}
+
+}
+
+trainees = {
+
+    1: {
+        "trainee_name": "Ahmed",
+        "age": 20,
+        "training_type": "Jumping",
+        "level": "Beginner",
+        "completed_classes": 5,
+        "package": 10,
+        "assigned_trainer": "Abdullah",
+        "assigned_horse": "Rahaf",
+        "license": False
+    },
+
+    2: {
+        "trainee_name": "Mohammed",
+        "age": 23,
+        "training_type": "Racing",
+        "level": "Intermediate",
+        "completed_classes": 18,
+        "package": 20,
+        "assigned_trainer": "Nawaf",
+        "assigned_horse": "Mazyon",
+        "license": False
+    },
+
+    3: {
+        "trainee_name": "Khalid",
+        "age": 25,
+        "training_type": "For fun",
+        "level": "Advanced",
+        "completed_classes": 35,
+        "package": 40,
+        "assigned_trainer": "Turki",
+        "assigned_horse": "Wadah",
+        "license": False
+    },
+
+    4: {
+        "trainee_name": "Saad",
+        "age": 21,
+        "training_type": "Jumping",
+        "level": "Advanced",
+        "completed_classes": 40,
+        "package": 40,
+        "assigned_trainer": "Abdulaziz",
+        "assigned_horse": "Shamekh",
+        "license": True
+    }
+
+}
+
+def register_trainee():
+
+    trainee_id = max(trainees.keys()) + 1
+
+    print("\n===== TRAINEE REGISTRATION =====")
+
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+
+    while True:
+
+        training_type = input(
+            "Choose training type (Jumping / Racing / For fun): "
+        ).title()
+
+        if training_type in ["Jumping", "Racing", "For Fun"]:
+            break
+
+        print("Invalid training type.")
+
+    while True:
+
+        package = int(input(
+            "\nChoose package:\n"
+            "10 Classes = 1500 SAR\n"
+            "20 Classes = 2900 SAR\n"
+            "40 Classes = 5400 SAR\n"
+            "Enter package: "
+        ))
+
+        if package in [10, 20, 40]:
+            break
+
+        print("Invalid package.")
+
+    print("\n===== INITIAL ASSESSMENT =====")
+    print("1. Beginner")
+    print("2. Intermediate")
+    print("3. Advanced")
+
+    while True:
+
+        assessment = input("Enter assessment result: ")
+
+        if assessment == "1":
+            level = "Beginner"
+            break
+
+        elif assessment == "2":
+            level = "Intermediate"
+            break
+
+        elif assessment == "3":
+            level = "Advanced"
+            break
+
+        else:
+            print("Invalid choice.")
+
+    trainer_name = assign_trainer(training_type, level)
+    horse_name = assign_horse(level)
+
+    trainees[trainee_id] = {
+
+        "trainee_name": name,
+        "age": age,
+        "training_type": training_type,
+        "package": package,
+        "level": level,
+        "completed_classes": 0,
+        "assigned_trainer": trainer_name,
+        "assigned_horse": horse_name,
+        "license": False
+    }
+
+    print("\nRegistration Successful.")
+    print("Application Accepted.")
+    print(f"Assigned Trainer: {trainer_name}")
+    print(f"Assigned Horse: {horse_name}")
+
+
+def assign_trainer(training_type, level):
+
+    for trainer in trainers.values():
+
+        score = trainer["performance_score"]
+
+        if level == "Beginner" and score < 80:
+
+            if training_type.lower() in trainer["training_type"].lower():
+                return trainer["trainer_name"]
+
+        elif level == "Intermediate" and 80 <= score <= 89:
+
+            if training_type.lower() in trainer["training_type"].lower():
+                return trainer["trainer_name"]
+
+        elif level == "Advanced" and score >= 90:
+
+            if training_type.lower() in trainer["training_type"].lower():
+                return trainer["trainer_name"]
+
+    return "No Trainer Available"
+
+
+
+def assign_horse(level):
+
+    for horse in horses.values():
+
+        horse_level = horse["horse_level"]
+
+        if level == "Beginner" and 60 <= horse_level <= 79:
+            return horse["horse_name"]
+
+        elif level == "Intermediate" and 80 <= horse_level <= 89:
+            return horse["horse_name"]
+
+        elif level == "Advanced" and horse_level >= 90:
+            return horse["horse_name"]
+
+    return "No Horse Available"
+
+
+
+def show_trainee_info(name):
+
+    for trainee in trainees.values():
+
+        if trainee["trainee_name"].lower() == name.lower():
+
+            print("\n===== MY INFORMATION =====")
+
+            print(f"Name: {trainee['trainee_name']}")
+            print(f"Age: {trainee['age']}")
+            print(f"Training Type: {trainee['training_type']}")
+            print(f"Level: {trainee['level']}")
+            print(f"Package: {trainee['package']} Classes")
+            print(f"Completed Classes: {trainee['completed_classes']}")
+            print(f"Assigned Trainer: {trainee['assigned_trainer']}")
+            print(f"Assigned Horse: {trainee['assigned_horse']}")
+
+            if trainee["license"]:
+                print("License Status: Licensed")
+
+            else:
+                print("License Status: Not Licensed")
+
+            break
+
+
+
+
+def show_progress(name):
+
+    for trainee in trainees.values():
+
+        if trainee["trainee_name"].lower() == name.lower():
+
+            progress = (
+                trainee["completed_classes"]
+                / trainee["package"]
+            ) * 100
+
+            remaining = (
+                trainee["package"]
+                - trainee["completed_classes"]
+            )
+
+            if remaining < 0:
+                remaining = 0
+
+            print("\n===== PROGRESS REPORT =====")
+            print(f"Completed Classes: {trainee['completed_classes']}")
+            print(f"Remaining Classes: {remaining}")
+            print(f"Progress: {progress:.0f}%")
+
+            break
+
+
+
+def complete_class(name):
+
+    for trainee in trainees.values():
+
+        if trainee["trainee_name"].lower() == name.lower():
+
+            trainee["completed_classes"] += 1
+
+            print("\nClass Completed Successfully.")
+
+            if trainee["completed_classes"] >= 40:
+
+                trainee["license"] = True
+
+                print("""
+=========================================
+Congratulations!
+You have earned your riding license.
+=========================================
+""")
+
+            break
+
+
+
+def trainee_menu():
+
+    while True:
+
+        print("\n===== TRAINEE MENU =====")
+        print("1. Register Trainee")
+        print("2. My Information")
+        print("3. Progress Report")
+        print("4. Complete Class")
+        print("5. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            register_trainee()
+
+        elif choice == "2":
+
+            name = input("Enter your name: ")
+            show_trainee_info(name)
+
+        elif choice == "3":
+
+            name = input("Enter your name: ")
+            show_progress(name)
+
+        elif choice == "4":
+
+            name = input("Enter your name: ")
+            complete_class(name)
+
+        elif choice == "5":
+            break
+
+        else:
+            print("Invalid choice.")
+trainee_menu()
+  
